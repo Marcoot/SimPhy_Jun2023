c302cb22142810f58389b6e71678b27215a2c085
Marcoot <marco.bosker@hva.nl>
2023-01-31 02:13:04 +0100

    Add project files.

---
+++ GameStates/PlayingState.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using SimPhy_Jun2021.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseProject.GameStates
+{
+    class PlayingState : GameObjectList
+    {
+        private PlayerCircle player;
+
+        /// <summary>
+        /// PlayState constructor which adds the different gameobjects and lists in the correct order of drawing.
+        /// </summary>
+        public PlayingState()
+        {
+            player = new PlayerCircle(new Vector2(400, 300), "circle");
+            Add(player);
+
+            // Add initialization logic here
+        }
+
+        /// <summary>
+        /// Updates the PlayState.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            // Add update logic here
+        }
+    }
+
+}
---
+++ SimPhy_Jun2022.cs
@@ -0,0 +1,23 @@
+﻿using BaseProject.GameStates;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BaseProject
+{
+    public class SimPhy_Jun2021 : GameEnvironment
+    {      
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            screen = new Point(800, 600);
+            ApplyResolutionSettings();
+
+            // TODO: use this.Content to load your game content here
+            GameStateManager.AddGameState("PlayingState", new PlayingState());
+            GameStateManager.SwitchTo("PlayingState");
+        }
+
+    }
+}
